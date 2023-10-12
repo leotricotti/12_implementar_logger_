4ddc5c7b64a3ee05c44c96d500aa4e1bddef9e89
Leonardo Tricotti <tricottileo@gmail.com>
2023-10-12 20:46:42 -0300

    commit

--- src/controllers/userCart.controller.js
+++ src/controllers/userCart.controller.js
@@ -1,10 +1,29 @@
 import { usersService } from "../repository/index.js";
+import CustomError from "../services/errors/CustomError.js";
+import EErrors from "../services/errors/enum.js";
+import { generateUserCartErrorInfo } from "../services/errors/info.js";
 
 //Ruta que agrega el id del carrito al usuario
 async function userCart(req, res) {
   const { cartId, email } = req.body;
   try {
+    if (!cartId || !email) {
+      CustomError.createError({
+        name: "Error de tipo de dato",
+        cause: generateUserCartErrorInfo(carts, EErrors.INVALID_TYPES_ERROR),
+        message: "Error al cargar el carrito",
+        code: EErrors.INVALID_TYPES_ERROR,
+      });
+    }
     const user = await usersService.getOneUser(email);
+    if (user.length === 0) {
+      CustomError.createError({
+        name: "Error de base de datos",
+        cause: generateUserCartErrorInfo(user, EErrors.DATABASE_ERROR),
+        message: "Error al cargar el carrito",
+        code: EErrors.DATABASE_ERROR,
+      });
+    }
     const userId = user[0]._id;
     const cartExist = user[0].carts.find((cart) => cart == cartId);
     if (!cartExist) {
@@ -14,10 +33,7 @@ async function userCart(req, res) {
       return false;
     }
   } catch (err) {
-    res.status(500).json({
-      message: "Error al agregar el carrito",
-      data: err,
-    });
+    next(err);
   }
 }
 
--- src/services/errors/info.js
+++ src/services/errors/info.js
@@ -93,9 +93,31 @@ const generateTicketErrorInfo = (ticket, errorType) => {
   return errorMessage;
 };
 
+const generateUserCartErrorInfo = (user, errorType) => {
+  let errorMessage = "";
+  switch (errorType) {
+    case EErrors.INVALID_TYPES_ERROR:
+      errorMessage = `One or more products have invalid types:
+        List of required types:
+        * email: string ${typeof user.email}
+        * cartId: string ${typeof user.cartId}
+      `;
+      break;
+    case EErrors.DATABASE_ERROR:
+      errorMessage = "Error al acceder a la base de datos";
+      break;
+    case EErrors.ROUTING_ERROR:
+    default:
+      errorMessage = "Error en la ruta";
+      break;
+  }
+  return errorMessage;
+};
+
 export {
   generateProductErrorInfo,
   generateCartErrorInfo,
   generateSessionErrorInfo,
   generateTicketErrorInfo,
+  generateUserCartErrorInfo,
 };
