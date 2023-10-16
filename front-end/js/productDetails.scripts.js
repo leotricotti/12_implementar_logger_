c9095868578d19db6f5594b215dd5c455359812d
Leonardo Tricotti <tricottileo@gmail.com>
2023-10-16 10:18:19 -0300

    commit

---
+++ front-end/js/productDetails.scripts.js
@@ -0,0 +1,5 @@
+// Esta función que cambiar la imagen principal del producto
+function changeImage(element) {
+  var main_prodcut_image = document.getElementById("main_product_image");
+  main_prodcut_image.src = element.src;
+}
